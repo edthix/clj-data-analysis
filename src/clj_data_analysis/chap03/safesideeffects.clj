11cc1826f78f1a4d1241e76c764a4e824f80cca5
Edham Arief Dawillah / edthix <edthix@gmail.com>
2014-11-24 21:28:59 +0800

    Safe side effects

--- src/clj_data_analysis/chap03/safesideeffects.clj
+++ src/clj_data_analysis/chap03/safesideeffects.clj
@@ -12,3 +12,49 @@
 (def a1 (agent :a1))
 (def a2 (agent :a2))
 
+(defn start-agents
+  "Start both agents on the same message function with different
+  sleep periods"
+  [msg a1-sleep a2-sleep]
+  (send a1 msg a1-sleep)
+  (send a2 msg a2-sleep))
+
+(defn debug
+  [msg]
+  (print (str msg \newline))
+  (.flush *out*))
+
+(defn starve-out
+  [tag sleep-for]
+  (let [retries (atom 0)]
+    (dosync
+     (let [c @counter]
+       (when-not (zero? @retries)
+         (debug (str ":starve-out " tag
+                     ", :try " @retries
+                     ", :counter " c)))
+       (swap! retries inc)
+       (Thread/sleep sleep-for)
+       (ref-set counter (inc c))
+       (send *agent* starve-out sleep-for)
+       tag))))
+
+;; a2 consistently gets starve out
+(start-agents starve-out 0 0)
+
+(defn debug! [msg]
+  (io! (print (str msg \newline)) (.flush *out*)))
+
+(defn starve-safe [tag sleep-for]
+  (let [retries (atom 0)]
+    (dosync (let [c @counter]
+              (swap! retries inc)
+              (Thread/sleep sleep-for)
+              (ref-set counter (inc c))))
+    (when-not (zero? @retries)
+      (debug! (str ":safe-starve " tag
+                   ", :try " @retries ", " @counter)))
+    (send *agent* starve-safe sleep-for)
+    tag))
+
+(start-agents starve-safe 0 0)

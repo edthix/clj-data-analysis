7801baeb1052a3c30ad66be4e32ceaeb8eb4ba28
Edham Arief / edthix <edthix@gmail.com>
2014-11-24 21:06:01 +0800

    data-src now is based on os (mac or windows)

--- src/clj_data_analysis/helpers.clj
+++ src/clj_data_analysis/helpers.clj
@@ -1,6 +1,9 @@
 (ns clj-data-analysis.helpers)
 
-(def data-src "C:\\xampp\\htdocs\\CLJ\\books\\clj-data-analysis\\src\\clj_data_analysis\\data\\")
+(def data-src
+  (if (re-matches #"Mac.*" (System/getProperty "os.name"))
+    "/Users/ed/public_html/clj/clj-data-analysis/src/clj_data_analysis/data/"
+    "C:\\xampp\\htdocs\\CLJ\\books\\clj-data-analysis\\src\\clj_data_analysis\\data\\"))
 
 (defn get-data-src
   ;; Get data source

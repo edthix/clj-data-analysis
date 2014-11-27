98af4597677ff95ba28f4b7709d7167fb73b30f3
Edham Arief Dawillah / edthix <edthix@gmail.com>
2014-11-28 02:34:11 +0800

    Incanter

---
+++ src/clj_data_analysis/chap04/incanter.clj
@@ -0,0 +1,20 @@
+(ns clj-data-analysis.chap04.incanter
+  (:use [incanter core datasets io optimize stats charts]
+        clj-data-analysis.helpers
+        [clj-data-analysis.chap03.stm :only [data-file]]))
+
+;; Read data and pull population/housing into matrix
+(def data (to-matrix
+           (sel (read-dataset data-file :header true)
+                :cols [:POP100 :HU100])))
+
+(def population (sel data :cols 0))
+population
+(def housing-units (sel data :cols 1))
+housing-units
+(def lm (linear-model housing-units population))
+
+(def plot (scatter-plot population housing-units :legend true))
+(add-lines plot population (:fitted lm))
+(view plot)
+

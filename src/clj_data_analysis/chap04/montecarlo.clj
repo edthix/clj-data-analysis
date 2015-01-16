d4897df477095f482e92899fa23ec75f14467103
Edham Arief / edthix <edthix@gmail.com>
2015-01-16 17:30:28 +0800

    Completed Partitioning Monte Carlo simulations with pmap

--- src/clj_data_analysis/chap04/montecarlo.clj
+++ src/clj_data_analysis/chap04/montecarlo.clj
@@ -23,12 +23,43 @@
    count))
 
 (defn mc-pi
-  ""
+  "Random points in a unit square that fall inside a circle"
   [n]
   (* 4.0 (/ (count-in-circle n) n)))
 
 (defn in-circle-flag
+  "Takes a point and return 1 if it's in the circle or 0 if not."
   [p]
   (if (<= (center-dist p) 1.0)
     1
     0))
+
+(defn mc-pi-pmap
+  "Add up to find number in circle"
+  [n]
+  (let [in-circle(->>
+                  (repeatedly n rand-point)
+                  (pmap in-circle-flag)
+                  (reduce + 0))]
+    (* 4.0 (/ in-circle n))))
+
+(defn mc-pi-part
+  "Sequence that tells how large each partition should be and have pmap walk across that."
+  ([n] (mc-pi-part 512 n))
+  ([chunk-size n]
+   (let [step (int
+               (Math/floor (float (/ n chunk-size))))
+         remainder (mod n chunk-size)
+         parts (lazy-seq
+                (cons remainder
+                      (repeat step chunk-size)))
+         in-circle (reduce + 0
+                           (pmap count-in-circle
+                                 parts))]
+     (* 4.0 (/ in-circle n)))))
+
+;; Test run
+(def chunk-size 4096)
+(def input-size 1000000)
+
+(quick-bench (mc-pi input-size)) ;; slow

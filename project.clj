721257be563bb834009f72c4b6918ae46de9104d
Edham Arief Dawillah / edthix <edthix@gmail.com>
2015-01-18 03:31:02 +0800

    Added data.csv

--- project.clj
+++ project.clj
@@ -4,11 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 ;;                  [incanter/incanter-core "1.5.1"]
-                 ;;                  [incanter/incanter-io "1.5.1"]
-                 ;;                  [incanter/incanter-charts "1.5.1"]
                  [incanter "1.5.1"]
                  [org.clojure/data.json "0.2.5"]
+                 [org.clojure/data.csv "0.1.2"]
                  [clj-diff "1.0.0-SNAPSHOT"]
                  [clj-time "0.9.0-beta1"]
                  [org.clojure/data.csv "0.1.2"]
